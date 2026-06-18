63c561363c39840a938894001ab919e7e649ce3c
Shaik Sohail <shaiksohaildnalabsindia@gmail.com>
2026-06-18 11:24:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 // config.js
 const CONFIG = {
-    // Your Google Apps Script Web App URL
+    // Previous endpoints
     API_URL: "https://script.google.com/macros/s/AKfycbxrtcZeaWfHYcTfEhidGXr3mxzmrUTaN-zliEJUGlbb8kBl48gby01-mEHxEuV2WlFzrA/exec",
+    SCANNER_API_URL: "https://script.google.com/macros/s/AKfycbyDie30z8_YE8HbZXobIKVXles9RFW3Xo5k1NE24DzrfnTZkfks6u6e4_ZKYFmumrrV/exec",
+    TRACKING_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz0eXdOvzB5zVGRNkQkCnCmLixnZdH3E-uXpZRkv4nIZfqMDmOcuU1DvcFGVshO3yyG/exec",
+    SCANNER_SAVE_URL: "https://script.google.com/macros/s/AKfycbwiTXXO1ypYhs-fykSl9apgTV4ZM9sGvdK5uKp-hkxGYcJjCJoaNZzsuXXFouq9ziY/exec",
     
-    // You can add other global variables here in the future
+    // NEW: Image Upload Endpoints
+    UPLOAD_TRACKING_URL: "https://script.google.com/macros/s/AKfycbxrAMIt5g_szYTWAXFAACOI7R-XpJiP9CS9bGEsX5bja552Ic-EoAJwbBSN99ZTwdyU_A/exec",
+    UPLOAD_PATIENT_URL: "https://script.google.com/macros/s/AKfycbxB5-Po-A-N70dR7Ie9VXP0QDHw1hFARAhaMTDoD4FbcNbDrPdh4W1nWootW1iepmtWbA/exec",
+    
     VERSION: "2.1.0"
 };
